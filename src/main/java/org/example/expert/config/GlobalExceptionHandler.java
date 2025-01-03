5766dcc244bfb7811b694c11631feec25b3b99e2
임PRO스터\su992 <su9926@naver.com>
2025-01-03 16:31:35 +0900

    레벨 3-3: 코드 클린업
    - 메서드명 수정(invalidRequestExceptionException -> handleInvalidRequestException)

--- src/main/java/org/example/expert/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/expert/config/GlobalExceptionHandler.java
@@ -15,7 +15,7 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(InvalidRequestException.class)
-	public ResponseEntity<Map<String, Object>> invalidRequestExceptionException(InvalidRequestException ex) {
+	public ResponseEntity<Map<String, Object>> handleInvalidRequestException(InvalidRequestException ex) {
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		return getErrorResponse(status, ex.getMessage());
 	}

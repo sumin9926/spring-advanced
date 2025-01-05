63854f7b1133c3a39e3c69b1a8d41e33f0e12fb1
임PRO스터\su992 <su9926@naver.com>
2025-01-06 01:42:57 +0900

    레벨 4-1: N+1 문제 개선하기

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,13 +1,21 @@
 package org.example.expert.domain.todo.repository;
 
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+	@EntityGraph(attributePaths = {"user"})
+	Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    int countById(Long todoId);
+	default Todo findByIdOrElseThrow(long id) {
+		return findById(id)
+			.orElseThrow(() -> new InvalidRequestException("Todo not found"));
+	}
+
+	int countById(Long todoId);
 }
